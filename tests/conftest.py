8f7cf5a642cdebd0d7b48a398707bdbdac8def7f
Sam Lock <sam@swlock.co.uk>
2023-07-04 12:26:34 +0100

    refactor conftest start_container func
    Signed-off-by: Sam Lock <sam@swlock.co.uk>

--- tests/conftest.py
+++ tests/conftest.py
@@ -64,11 +64,6 @@ def start_container(client_type, listener, tmp_path_factory):
     container.with_volume_mapping(policy_dir, "/policies")
     container.with_env("CERBOS_NO_TELEMETRY", "1")
 
-    commands = {
-        "http": "server --set=server.httpListenAddr=unix://{socket_path} --set=server.udsFileMode=0o777 --set=schema.enforcement=reject",
-        "grpc": "server --set=server.grpcListenAddr=unix://{socket_path} --set=server.udsFileMode=0o777 --set=schema.enforcement=reject",
-    }
-
     if listener == "http":
         container.with_command("server --set=schema.enforcement=reject")
         container.start()
@@ -77,11 +72,16 @@ def start_container(client_type, listener, tmp_path_factory):
         host = container.http_host() if client_type == "http" else container.grpc_host()
     else:
         sock_dir = tmp_path_factory.mktemp("socket")
-        socket_path = f"/socket/{client_type}.sock"
-        host = f"unix+http://{sock_dir}/{client_type}.sock"
+
+        if client_type == "http":
+            host = f"unix+http://{sock_dir}/http.sock"
+        else:
+            host = f"unix:{sock_dir}/grpc.sock"
 
         container.with_volume_mapping(sock_dir, "/socket", "rw")
-        container.with_command(commands[client_type].format(socket_path=socket_path))
+        container.with_command(
+            f"server --set=server.{client_type}ListenAddr=unix:/socket/{client_type}.sock --set=server.udsFileMode=0o777 --set=schema.enforcement=reject"
+        )
         container.start()
         container.wait_until_ready()
 

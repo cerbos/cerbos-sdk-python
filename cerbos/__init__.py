92a1b2786c8a93f76350f80a78090f833c8cbaa3
Sam Lock <sam@swlock.co.uk>
2024-01-15 10:37:00 +0000

    swap manual gen cerbos proto code for buf SDK
    Signed-off-by: Sam Lock <sam@swlock.co.uk>

--- cerbos/__init__.py
+++ cerbos/__init__.py
@@ -2,5 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import importlib.metadata
+import pkgutil
 
 __version__ = importlib.metadata.version(__package__ or __name__)
+__path__ = pkgutil.extend_path(__path__, __name__)

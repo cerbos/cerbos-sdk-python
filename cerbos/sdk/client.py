37aee23190168511ae7ce3519a31b4b6579eb1d0
Charith Ellawala <charith@cerbos.dev>
2025-01-03 16:43:16 +0000

    Update copyright header
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- cerbos/sdk/client.py
+++ cerbos/sdk/client.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2022 Zenauth Ltd.
+# Copyright 2021-2025 Zenauth Ltd.
 # SPDX-License-Identifier: Apache-2.0
 
 from cerbos.sdk._async._http import AsyncCerbosClient as AsyncCerbosClient

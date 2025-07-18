783bcc01d4d477e7133e52b5cc36b4cd3e10a01c
Charith Ellawala <charith@cerbos.dev>
2025-07-18 11:40:16 +0100

    Lint
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- src/cerbos/sdk/_sync/_hub/_interceptors.py
+++ src/cerbos/sdk/_sync/_hub/_interceptors.py
@@ -1,6 +1,6 @@
 # Copyright 2021-2025 Zenauth Ltd.
 # SPDX-License-Identifier: Apache-2.0
-from typing import Any, Callable, List, NamedTuple, Optional, Union
+from typing import Any, Callable, List, NamedTuple, Optional
 
 import grpc
 
